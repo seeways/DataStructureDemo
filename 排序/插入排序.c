8320c04975621ae4df0ecdce4346f744efcb28f0
Taoyuan <1876665310@qq.com>
2019-08-10 00:15:24 +0800

    简单排序

---
+++ 排序/插入排序.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+
+/**
+arg1: 数组
+arg2: 数组个数
+
+*/
+void insertSort(int arr[], int n)
+{
+	int temp, i, j;
+	for (i = 1; i < n; ++i)
+	{
+		temp = arr[i];
+		j = i-1;
+		while(j>=0&&temp<arr[j])
+		{
+			arr[j+1] = arr[j];
+			--j;
+		}
+		arr[j+1] = temp;
+
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	int arr[7] = {2,3,4,1,7,5,6};
+	insertSort(arr, 7);
+
+	for (int i = 0; i < 7; ++i)
+	{
+		printf("%d ", arr[i]);
+	}
+	getchar();
+	return 0;
+}
