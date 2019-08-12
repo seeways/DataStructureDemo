06df0717ee6b714491870a00f41e7becf2a6e822
seeways <1876665310@qq.com>
2019-08-12 16:48:10 +0800

    冒泡排序

---
+++ 排序算法/冒泡排序.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+void bubleSort(int arr[], int n)
+{
+	int i, j, flag;
+	int temp;
+	for (i = n-1; i >0; --i)
+	{
+		flag = 0;
+		for (j = 1; j<=i; ++j)
+		{
+			if (arr[j-1] > arr[j])
+			{
+				temp = arr[j];
+				arr[j] = arr[j-1];
+				arr[j-1] = temp;
+				flag = 1;
+			}
+		}
+		if (flag == 0) return;
+	}
+}
+
+int main(int argc, char const *argv[])
+{
+	int arr[7] = {2,3,4,1,7,5,6};
+	bubleSort(arr, 7);
+
+	for (int i = 0; i < 7; ++i)
+	{
+		printf("%d ", arr[i]);
+	}
+	getchar();
+	return 0;
+}
