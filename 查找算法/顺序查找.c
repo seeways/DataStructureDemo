271ad429111a5f89e88f0e6ec5ee2cdcc76e999e
seeways <1876665310@qq.com>
2019-08-12 16:49:23 +0800

    顺序查找

---
+++ 查找算法/顺序查找.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// 查找函数
+int Search(int arr[], int n, int key)
+{
+	while(key != arr[n-1])
+		n--;
+	return n;
+}
+
+
+int main(int argc, char const *argv[])
+{
+	// 一个顺序表
+	int arr[7] = {1,2,3,4,5,6,7};
+	for (int i = 0; i < 7; ++i)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+
+	// 输入一个要查询的关键字
+	int key;
+	printf("input key:\n");
+	scanf("%d", &key);
+
+	// 查找关键字在数组中的下标
+	int location = Search(arr,7,key);
+
+	int n = 0;
+	while(key != arr[n-1])
+		n--;
+	if (n < 0)
+	{
+		printf("Search Failed! \n");
+	}
+	else{
+		printf("array index: %d\n", n);
+	}
+	
+	system("pause");
+	return 0;
+}
+
