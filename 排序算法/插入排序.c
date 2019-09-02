3ccffa597d9cc6c6c232865c34fada13fc0d0a45
seeways <1876665310@qq.com>
2019-09-02 11:40:16 +0800

    插入排序

--- 排序算法/插入排序.c
+++ 排序算法/插入排序.c
@@ -2,30 +2,29 @@
 
 void insertSort(int arr[], int n)
 {
-	int temp, i, j;
-	for (i = 1; i < n; ++i)
-	{
-		temp = arr[i];
-		j = i-1;
-		while(j>=0&&temp<arr[j])
-		{
-			arr[j+1] = arr[j];
-			--j;
-		}
-		arr[j+1] = temp;
-
-	}
+    int temp, i, j;
+    for (i = 1; i < n; ++i)
+    {
+        temp = arr[i];
+        j = i - 1;
+        while(j >= 0 && temp < arr[j])
+        {
+            arr[j + 1] = arr[j];
+            --j;
+        }
+        arr[j + 1] = temp;
+        for (int i = 0; i < n; ++i)
+        {
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+    }
 }
 
 int main(int argc, char const *argv[])
 {
-	int arr[7] = {2,3,4,1,7,5,6};
-	insertSort(arr, 7);
-
-	for (int i = 0; i < 7; ++i)
-	{
-		printf("%d ", arr[i]);
-	}
-	getchar();
-	return 0;
+    int arr[8] = {3, 1, 7, 5, 2, 4, 9, 6};
+    insertSort(arr, 8);
+    getchar();
+    return 0;
 }
