230cf63e684821553a68f27199e669fb5785de88
seeways <1876665310@qq.com>
2019-09-02 18:19:27 +0800

    表插入排序

--- 排序算法/表插入排序算法.c
+++ 排序算法/表插入排序算法.c
@@ -1,54 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define SIZE 6
 
-void BInsertSort(int arr[], int n)
+
+typedef struct
 {
-    int low = 0, high = 0, mid;
-    int temp = 0;
-    for (int i = 1; i < n; i++)
+    int rc;//记录项
+    int next;//指针项，由于在数组中，所以只需要记录下一个结点所在数组位置的下标即可。
+} SLNode;
+typedef struct
+{
+    SLNode r[SIZE];//存储记录的链表
+    int length;//记录当前链表长度
+} SLinkListType;
+//重新排列函数
+void Arrange(SLinkListType *SL)
+{
+    //令 p 指向当前要排列的记录
+    int p = SL->r[0].next;
+    for (int i = 1; i < SL->length; i++)
     {
-        low = 0;
-        high = i - 1;
-        temp = arr[i];
-
-        //采用折半查找法判断插入位置，最终变量 low 表示插入位置
-        while (low <= high)
+        //如果条件成立，证明原来的数据已经移动，需要通过不断找 next 域，找到其真正的位置
+        while (p < i)
         {
-            mid = (low + high) / 2; // 取上
-            // 中位 > 顺序位，则取低位，反之取高位
-            if (arr[mid] > temp)
-            {
-                high = mid - 1;
-            }
-            else
-            {
-                low = mid + 1;
-            }
+            p = SL->r[p].next;
         }
-
-        //有序表中插入位置后的元素统一后移
-        for (int j = i; j > low; j--)
+        //找到之后，令 q 指针指向其链表的下一个记录所在的位置
+        int q = SL->r[p].next;
+        //条件成立，证明需要同下标为 i 的记录进行位置交换
+        if (p != i)
         {
-            arr[j] = arr[j - 1];
+            SLNode t;
+            t = SL->r[p];
+            SL->r[p] = SL->r[i];
+            SL->r[i] = t;
+            //交换完成后，该变 next 的值，便于后期遍历
+            SL->r[i].next = p;
         }
-        arr[low] = temp; //插入元素
-
-        // 打印排序后的数组
-        printf("%d: ", i);
-        for(int k = 0; k < n; k++)
-        {
-            printf("%d ", arr[k]);
-        }
-        printf("\n");
+        //最后令 p 指向下一条记录
+        p = q;
     }
-
 }
 
-
-int main(int argc, char const *argv[])
+int main(int argc, const char *argv[])
 {
-    int arr[8] = {3, 1, 7, 5, 2, 4, 9, 6};
-    BInsertSort(arr, 8);
+
+    SLinkListType *SL = (SLinkListType *)malloc(sizeof(SLinkListType));
+    SL->length = 6;
+    SL->r[0].rc = 0;
+    SL->r[0].next = 4;
+
+    SL->r[1].rc = 49;
+    SL->r[1].next = 3;
+
+    SL->r[2].rc = 38;
+    SL->r[2].next = 1;
+
+    SL->r[3].rc = 76;
+    SL->r[3].next = 0;
+
+    SL->r[4].rc = 13;
+    SL->r[4].next = 5;
+
+    SL->r[5].rc = 27;
+    SL->r[5].next = 2;
+
+    printf("before:\n");
+    for (int i = 1; i < 6; i++)
+    {
+        printf("%d ", SL->r[i].rc);
+    }
+    printf("\n");
+
+    Arrange(SL);
+
+    printf("after:\n");
+    for (int i = 1; i < 6; i++)
+    {
+        printf("%d ", SL->r[i].rc);
+    }
     getchar();
     return 0;
 }
-
